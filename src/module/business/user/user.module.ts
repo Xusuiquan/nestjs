b1c3d1b2703ead658b346cf426364ab94f181beb
wxxusuiquan <wxxusuiquan@gf.com.cn>
2024-08-06 18:09:49 +0800

    feat: 新增jwt模块

--- src/module/business/user/user.module.ts
+++ src/module/business/user/user.module.ts
@@ -3,11 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
     imports: [
-        TypeOrmModule.forFeature([UserEntity])
+        TypeOrmModule.forFeature([UserEntity]),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (config: ConfigService) => ({
+                secret: config.get('jwt.secret')
+            }),
+            inject: [ConfigService]
+        }),
     ],
     controllers: [UserController],
     providers: [UserService],
